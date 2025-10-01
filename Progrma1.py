61c6f37a7aab14eca0e27afa4c44f3164978bd33
a385940-dot <a385940@uach.mx>
2025-10-01 09:17:41 -0600

    Create Progrma1.py

---
+++ Progrma1.py
@@ -0,0 +1,6 @@
+# Este es un script de python de prubea para el respositorio
+def saludo (nombre):
+  return "Hola"+nombre
+
+# Se invoca a la función saludo para saludar a alguien llamado Alexa
+saludo("Alexa")
